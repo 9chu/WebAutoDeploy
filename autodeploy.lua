094b12637fa56a82a9b07911b145bfa803ac6ef6
chu <1871361697@qq.com>
2015-11-24 17:41:08 +0800

    Update autodeploy.lua

--- autodeploy.lua
+++ autodeploy.lua
@@ -8,6 +8,7 @@ local config = {
     verify_key = "YOUR_VERIFY_KEY_HERE",
     deploy_path = "/TO/DEPLOY/PATH",
     deploy_url = "http://GITLAB_USERNAME:GITLAB_PASSWD@WWW.MYGITLAB.COM/YOUR_REPO.git",
+    timeout = 30 * 1000,  -- 30s
 }
 
 local function say_json(msg)
@@ -39,7 +40,7 @@ else
         else
             -- pull operation(non-blocking)
             local shell_cmd = "cd "..config.deploy_path.."; git reset --hard origin/master; git clean -f; git pull "..config.deploy_url
-            local status, out, err = shell.execute(shell_cmd)
+            local status, out, err = shell.execute(shell_cmd, {timeout = config.timeout})
 
             -- print result
             if status ~= 0 then
